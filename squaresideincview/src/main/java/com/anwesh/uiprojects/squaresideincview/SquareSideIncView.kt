6ef6df35b3551b2911299ce1f7fbbe2a4479e4ee
Anwesh43 <anweshthecool0@gmail.com>
2018-07-30 01:51:30 +0530

    importing libraries and defining number of nodes

--- squaresideincview/src/main/java/com/anwesh/uiprojects/squaresideincview/SquareSideIncView.kt
+++ squaresideincview/src/main/java/com/anwesh/uiprojects/squaresideincview/SquareSideIncView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.squaresideincview
 /**
  * Created by anweshmishra on 30/07/18.
  */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
