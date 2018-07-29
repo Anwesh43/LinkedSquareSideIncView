2aeea12be659d55ff6d90a5737d210d982177bd3
Anwesh43 <anweshthecool0@gmail.com>
2018-07-30 02:23:54 +0530

    adding LinkedSSI class

--- squaresideincview/src/main/java/com/anwesh/uiprojects/squaresideincview/SquareSideIncView.kt
+++ squaresideincview/src/main/java/com/anwesh/uiprojects/squaresideincview/SquareSideIncView.kt
@@ -147,4 +147,28 @@ class SquareSideIncView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedSSI(var i : Int) {
+
+        private var curr : SSINode = SSINode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scale ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
