be357893e264941a4db7bf83b700c73c3d3e2355
Anwesh43 <anweshthecool0@gmail.com>
2018-07-30 02:31:41 +0530

    using view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquaresideincview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquaresideincview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquaresideincview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squaresideincview.SquareSideIncView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareSideIncView.create(this)
     }
 }
